df18fe1edad8f03fd0145ae1b5b8d1e70742d089
AadityaThapa <aadityathapa.np@gmail.com>
2024-07-08 19:10:18 +0545

    replaced ws with socket.io

--- app/app.js
+++ app/app.js
@@ -1,24 +1,27 @@
-// Initialize client side web socket
-const socket = new WebSocket("ws://localhost:3000");
+// Initialize client side socket
+const socket = new io("ws://localhost:3500");
 
 // Select the html elements
 const input = document.querySelector("input");
 const form = document.querySelector("form");
 const ul = document.querySelector("ul");
 
-// Takes input from the form and sends message to the websocket.
+// Takes input from the form and sends message to the socket.
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
 	if (input.value) {
-		socket.send(input.value);
+		socket.emit("message", input.value);
 		input.value = "";
 	}
 	input.focus();
 });
 
-// Listen for messages
-socket.addEventListener("message", ({ data }) => {
+// Listen for messages and takes the data
+socket.on("message", (data) => {
+	// create a new li element
 	const li = document.createElement("li");
+	// set's the li element's value to data
 	li.textContent = data;
+	// add that li to the ul
 	ul.appendChild(li);
 });
--- server/index.js
+++ server/index.js
@@ -1,20 +1,39 @@
-// Websocket module
-const ws = require("ws");
+// importing nodejs built in http server and socket.io
+import { createServer } from "node:http";
+import { Server } from "socket.io";
 
-// Creates a server at port 3000
-const server = new ws.Server({
-	port: 3000,
+// Creates a http server
+const httpServer = createServer();
+const port = 3500;
+
+const io = new Server(httpServer, {
+	// Cross-Origin Resource Sharing
+	// what i understood is that we need this so we can access resources from a different domains or ports
+	// if the client and server is in different domains then i need to specify the client domain in here
+	cors: {
+		origin:
+			process.env.NODE_ENV === "production"
+				? false
+				: ["http://localhost:5500", "http://127.0.0.1:5500"],
+		// ? if true
+		// : if false
+		// if our server is in prod mode then returns false meaning no access to the socket (i guess??)
+		// else if our server is in dev mode then it gives localhost:5500 access to the socket
+		// liveserver extension uses localhost:5500 as it's default port
+	},
 });
 
 // Once we have "connection" established, we listen for sockets.
-server.on("connection", (socket) => {
-	// listen for a message, and take that message.
-	socket.on("message", (message) => {
-		// Buffer in Node.js is used to perform operations on raw binary data.
-		const buffer = Buffer.from(message);
-		// turns binary data into human readable string.
-		console.log(buffer.toString());
+io.on("connection", (socket) => {
+	console.log(`User ${socket.id} connected`);
+	// listen for a message, and runs the callback function
+	socket.on("message", (data) => {
+		console.log(data);
 		// Send that message back as a string.
-		socket.send(`${message}`);
+		io.emit("message", `${socket.id.substring(0, 5)}: ${data}`);
 	});
+});
+
+httpServer.listen(port, () => {
+	console.log(`listening on port ${port}`);
 });
