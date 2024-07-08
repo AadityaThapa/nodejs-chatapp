1164968841f324b708c120fc1cb3d20e81a28b5a
AadityaThapa <aadityathapa.np@gmail.com>
2024-07-08 20:05:03 +0545

    added express

--- server/index.js
+++ server/index.js
@@ -1,12 +1,27 @@
-// importing nodejs built in http server and socket.io
-import { createServer } from "node:http";
+// importing express and socket.io
+import express from "express";
 import { Server } from "socket.io";
+// nodejs modules
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
-// Creates a http server
-const httpServer = createServer();
-const port = 3500;
+// making a workaround cause __dirname isn't defined on es modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const io = new Server(httpServer, {
+// setting up the express server
+const app = express();
+const PORT = process.env.PORT || 3500;
+
+// Using the static assets
+app.use(express.static(path.join(__dirname, "public")));
+
+// Listens for connections on port
+const expressServer = app.listen(PORT, () => {
+	console.log(`listening on port ${PORT}`);
+});
+
+const io = new Server(expressServer, {
 	// Cross-Origin Resource Sharing
 	// what i understood is that we need this so we can access resources from a different domains or ports
 	// if the client and server is in different domains then i need to specify the client domain in here
@@ -15,7 +30,7 @@ const io = new Server(httpServer, {
 			process.env.NODE_ENV === "production"
 				? false
 				: ["http://localhost:5500", "http://127.0.0.1:5500"],
-		// ? if true
+		// ? if true [note: if we are in prod then replace the false value with the client's domain]
 		// : if false
 		// if our server is in prod mode then returns false meaning no access to the socket (i guess??)
 		// else if our server is in dev mode then it gives localhost:5500 access to the socket
@@ -28,12 +43,9 @@ io.on("connection", (socket) => {
 	console.log(`User ${socket.id} connected`);
 	// listen for a message, and runs the callback function
 	socket.on("message", (data) => {
-		console.log(data);
+		// buffer is a nodejs module which deals with binary data
+		console.log(`${socket.id}: ${Buffer.from(data)}`);
 		// Send that message back as a string.
 		io.emit("message", `${socket.id.substring(0, 5)}: ${data}`);
 	});
-});
-
-httpServer.listen(port, () => {
-	console.log(`listening on port ${port}`);
 });
